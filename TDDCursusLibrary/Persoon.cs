27b8dde7ed06541964f2841d8caec3c0c31284fb
davyraes <davy-raes@telenet.be>
2017-11-27 11:03:49 +0100

    eindoefening

---
+++ TDDCursusLibrary/Persoon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCursusLibrary
+{
+    public class Persoon
+    {
+        private List<string> Voornamen { get; set; }
+
+        public Persoon( List<string> voornamen)
+        {
+            if (voornamen.Count == 0)
+                throw new ArgumentException();
+            foreach (string voornaam in voornamen)
+            {
+                if (voornaam == null)
+                    throw new ArgumentException();
+                if (voornaam.ToArray().Count() == 0)
+                    throw new ArgumentException();
+                if (voornamen.IndexOf(voornaam) != voornamen.LastIndexOf(voornaam))
+                    throw new ArgumentException();
+            }
+            Voornamen = voornamen; 
+        }
+        public override string ToString()
+        {
+            var voornamen = new StringBuilder();
+            foreach (var voornaam in Voornamen)
+            {
+                voornamen.Append(voornaam);
+                voornamen.Append(" ");
+            }
+             voornamen.Remove(voornamen.Length - 1, 1);
+            return voornamen.ToString();
+        }
+    }
+}
---
+++ TDDCursusLibraryTest/PersoonTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TDDCursusLibrary;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDCursusLibraryTest
+{
+    [TestClass]
+    public class PersoonTest
+    {
+        [TestMethod]
+        public void EenPersoonIsJuistIgegeven()
+        { 
+            new Persoon(new List<string> { "davy","andre","hendrik" });
+        }
+        [TestMethod,ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMoetMinstens1VoornaamHebben()
+        {
+            new Persoon(new List<string> { });
+        }
+        [TestMethod,ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonKanNiet2MaalDezelfdeVoornaamHebben()
+        {
+            new Persoon(new List<string> { "davy", "andre", "davy" });
+        }
+        [TestMethod,ExpectedException(typeof(ArgumentException))]
+        public void ElkeVoornaamVanPersoonMoetMinstens1TekenBevatten()
+        {
+            new Persoon(new List<string> { "davy", "" });
+        }
+        [TestMethod,ExpectedException(typeof(ArgumentException))]
+        public void VoornamenMogenNietNullZijn()
+        {
+            new Persoon(new List<string> { null });
+        }
+        [TestMethod]
+        public void ToStringMoetDeVoornamenTerugGevenGescheidenDoorEenSpatie()
+        {
+            var persoon = new Persoon(new List<string> { "davy", "andre", "hendrik" });
+            Assert.AreEqual("davy andre hendrik", persoon.ToString());
+        }
+    }
+}
